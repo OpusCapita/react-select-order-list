21d8fb96fe1ced7511607b5daabafb3bde25a0b4
Sami Karjalainen <sami.karjalainen@opuscapita.com>
2018-02-20 21:46:46 +0200

    Update example

--- src_docs/index.jsx
+++ src_docs/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { AppContainer } from 'react-hot-loader';
-import ExampleContainer from './components/example.container';
+import ExampleContainer from './containers/example.container';
 import './app.component.scss';
-import './favicon.ico';
+import './images/favicon.ico';
 
 const renderApp = (Component) => {
   render(
@@ -22,8 +22,8 @@ renderApp(ExampleContainer);
 // Webpack Hot Module Replacement API
 /* eslint-disable global-require */
 if (module.hot) {
-  module.hot.accept('./components/example.container', () => {
-    const Comp = require('./components/example.container').default;
+  module.hot.accept('./containers/example.container', () => {
+    const Comp = require('./containers/example.container').default;
     renderApp(Comp);
   });
 }
