4200fd8c1d2c4a1d164d3559ed4437dfd51bb6cd
lukasdt <lukasz.frandt@cybercom.com>
2018-03-08 14:37:59 +0100

    Moving items styles improvements

--- src/selected-data-list/selected-data-list.component.jsx
+++ src/selected-data-list/selected-data-list.component.jsx
@@ -1,17 +1,16 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { Icon } from '@opuscapita/react-icons';
 import ScrollBar from '@opuscapita/react-perfect-scrollbar';
-import {
-  SortableContainer,
-  SortableElement,
-} from 'react-sortable-hoc';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 
-const SortableItem = SortableElement(({ value, handleItemRemove }) => (
+const SortableItem = SortableElement(({ value, handleItemRemove, mouseDown }) => (
   <div
     key={value.value}
     className={`oc-select-order-list-selected-data-item${value.isLocked ? ' locked' : ''}`}
+    onMouseDown={mouseDown}
   >
     <span className="oc-select-order-list-selected-data-item-text">
       {value.label}
@@ -36,6 +35,7 @@ const SortableItem = SortableElement(({ value, handleItemRemove }) => (
     }
     {!value.isLocked &&
       <Icon
+        id="oc-icon-remove"
         type="indicator"
         name="remove"
         className="oc-select-order-list-remove-icon"
@@ -47,7 +47,7 @@ const SortableItem = SortableElement(({ value, handleItemRemove }) => (
   </div>
 ));
 
-const SortableList = SortableContainer(({ items, handleItemRemove }) => (
+const SortableList = SortableContainer(({ items, handleItemRemove, mouseDown }) => (
   <div className="oc-select-order-list-selected-data-list">
     <ScrollBar>
       {items.map((value, index) => (
@@ -57,6 +57,7 @@ const SortableList = SortableContainer(({ items, handleItemRemove }) => (
           value={value}
           disabled={value.isLocked}
           handleItemRemove={handleItemRemove(value)}
+          mouseDown={mouseDown}
         />
       ))}
     </ScrollBar>
@@ -74,17 +75,26 @@ export default class SelectedDataList extends React.PureComponent {
     this.props.onRemoveItem(item);
   }
 
+  mouseDown = (e) => {
+    e.preventDefault();
+  }
+
+  handelMouseDown = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+  }
   shouldCancelStart = (e) => {
-    if (e.target.className.baseVal && e.target.className.baseVal.indexOf('oc-icon-remove') !== -1) {
+    if (e.target.id === 'oc-icon-remove' || (e.target.className.baseVal && e.target.className.baseVal.indexOf('oc-icon-remove') !== -1)) {
       return true;
     }
     return false;
   }
-
   render() {
     return (
       <SortableList
         handleItemRemove={this.handleItemRemove}
+        mouseDown={this.mouseDown}
         items={this.props.items}
         onSortEnd={this.props.onSortChange}
         shouldCancelStart={this.shouldCancelStart}
